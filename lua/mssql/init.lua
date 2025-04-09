95bf05a46b8a38a47e0116b950cc2ba8d53a584b
Kurren <kurren.nischal@codis.co.uk>
2025-04-09 15:59:06 +0100

    Constructing the download job

--- lua/mssql/init.lua
+++ lua/mssql/init.lua
@@ -41,10 +41,64 @@ local function get_tools_download_url()
 	return url
 end
 
-local function download_tools(url, target_folder)
-	-- delete any existing download folder, download, unzip and write the most recent url to the config
-	-- TODO: finish this
+-- delete any existing download folder, download, unzip and write the most recent url to the config
+local function download_tools(url, data_folder)
+	local target_folder = data_folder .. "/sqltools"
+
+	local job
+	if jit.os == "Windows" then
+		local temp_file = data_folder .. "/temp.zip"
+		-- Turn off the progress bar to speed up the download
+		job = {
+			"powershell",
+			"-Command",
+			string.format(
+				[[
+          $ProgressPreference = 'SilentlyContinue'
+          Invoke-WebRequest %s -OutFile "%s"
+          if (Test-Path -LiteralPath "%s") { Remove-Item -LiteralPath "%s" -Recurse }
+          Expand-Archive "%s" "%s"
+          Remove-Item "%s"
+          $ProgressPreference = 'Continue'
+        ]],
+				url,
+				temp_file,
+				target_folder,
+				target_folder,
+				temp_file,
+				target_folder,
+				temp_file
+			),
+		}
+	else
+		local temp_file = data_folder .. "/temp.gz"
+		job = {
+			"bash",
+			"-c",
+			string.format(
+				[[
+        curl -L "%s" -o "%s"
+        rm -rf "%s"
+        mkdir "%s"
+        tar -xzf "%s" -C "%s"
+        rm "%s"
+      ]],
+				url,
+				temp_file,
+				target_folder,
+				target_folder,
+				temp_file,
+				target_folder,
+				temp_file
+			),
+		}
+	end
+
+	return job
 end
+-- test lines
+vim.opt.rtp:append("C:/dev/mssql.nvim/")
+print(download_tools(get_tools_download_url(), "C:/dev/mssql.nvim/folder with spaces"))
 
 local M = {}
 
@@ -56,7 +110,13 @@ function M.setup(opts)
 	M.opts = opts or {}
 
 	-- if the opts specify a tools file path, don't download.
-	if not opts.tools_file then
+	if opts.tools_file then
+		local file = io.open(opts.tools_file, "r")
+		if not file then
+			error("No sql tools file found at " .. opts.tools_file)
+		end
+		file:close()
+	else
 		local data_dir = opts.data_dir or (vim.fn.stdpath("data") .. "/mssql.nvim")
 		local config = read_json_file(data_dir .. "/config.json")
 		local download_url = get_tools_download_url()
@@ -67,8 +127,5 @@ function M.setup(opts)
 		end
 	end
 end
-
--- test lines
-vim.opt.rtp:append("C:/dev/mssql.nvim/")
 
 return M
