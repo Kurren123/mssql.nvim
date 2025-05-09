63625b14a2f71d717d62af4df7819c0237789534
Kurren Nischal <kurren.nischal@codis.co.uk>
2025-05-09 09:00:18 +0100

    test not working

--- tests/file_with_space_spec.lua
+++ tests/file_with_space_spec.lua
@@ -5,15 +5,16 @@ local utils = require("mssql.utils")
 return {
 	test_name = "Should be able to connect with a filename with spaces",
 	run_test_async = function()
-		vim.cmd("edit tests/filename with spaces")
-		test_utils.ui_select_fake("TestDbB")
-		mssql.switch_database()
+		vim.cmd('edit "tests/filename\\ with\\ spaces.sql"')
+		test_utils.ui_select_fake("master")
+		mssql.connect()
+
+		utils.defer_async(2000)
+		mssql.refresh_intellisense_cache()
 		local client = vim.lsp.get_clients({ name = "mssql_ls", bufnr = 0 })[1]
 		local buf = vim.api.nvim_get_current_buf()
 
-		-- The connect event is sent, then the intelliSenseReady event.
-		-- Wait for the intelliSenseReady event as this means the connection was successful and
-		local result, err = utils.wait_for_notification_async(buf, client, "textDocument/intelliSenseReady", 30000)
+		local result, err = utils.wait_for_notification_async(buf, client, "textDocument/intelliSenseReady", 5000)
 		if err then
 			error(err.message)
 		end
